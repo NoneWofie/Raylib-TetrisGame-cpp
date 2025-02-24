0a0e735893050145847dd2269c424ebcb4fc26b4
REUBEN ROSHAN <lelopopsinger@gmail.com>
2025-02-25 04:39:38 +0800

    blocks centered

--- src/include/Block.hpp
+++ src/include/Block.hpp
@@ -10,9 +10,13 @@ public:
   void Draw();
   int id; // Each block type has a unique id
   std::map<int, std::vector<Position>> cells;
+  void Move(int rows, int columns);
 
 private:
   int cellSize;
   int rotationState;
   std::vector<Color> colors;
+  int rowOffset;
+  int columnOffset;
+  std::vector<Position> GetCellPosition();
 };
